e18b923cf4804bc9b2331148e9f8b2dd7334cb54
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-09-24 13:34:35 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewHelpers.Models;
+
+namespace ViewHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet] // Url'den çağrılacak
+        public IActionResult Kaydet()
+        {
+            return View();
+        }
+
+        [HttpPost] // form'dan çağrılacak...
+        public IActionResult Save(Kullanici model)
+        {
+
+            return View();
+        }
+        // Default her zaman HTTPGET'tir
+        public IActionResult Ekle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Ekle(Kullanici model)
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Kullanici model)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Models/Kullanici.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewHelpers.Models
+{
+    public class Kullanici
+    {
+        //DataAnnotation yöntemi ile helper metotlarımız burada eklediğimiz özellikleri html olarka render eder..
+        [Required(ErrorMessage ="Kullanıcı Adı Zorunludur")]
+        [Display(Name ="Kullanıcı Adınız")]
+        public string KullaniciAdi { get; set; }
+
+        [Required(ErrorMessage ="Şifre Zorunludur")]
+        [Display(Name = "Şifreniz")]
+        public string Sifre { get; set; }
+    }
+}
